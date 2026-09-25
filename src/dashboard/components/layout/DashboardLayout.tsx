bb7b01d451ad0c94b9e5cf2dcb59faf25a888768
Chaitanya Shinde <113242662+chaitanya-92@users.noreply.github.com>
2026-09-26 00:05:29 +0530

    refactor: dashboard: replace collapsible sidebar with animated rail

--- src/dashboard/components/layout/DashboardLayout.tsx
+++ src/dashboard/components/layout/DashboardLayout.tsx
@@ -1,28 +1,21 @@
 import { Outlet } from "react-router-dom";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { DashboardNavbar } from "./DashboardNavbar";
 import { DashboardSidebar } from "./DashboardSidebar";
 
 export function DashboardLayout() {
   return (
-    <SidebarProvider
-      defaultOpen={false}
-      style={
-        {
-          "--sidebar-width": "296px",
-          "--sidebar-width-icon": "72px",
-        } as React.CSSProperties
-      }
-    >
+    <div className="min-h-screen bg-[#f7f9fc]">
       <DashboardSidebar />
-      <SidebarInset>
+
+      <div className="min-h-screen pl-[76px]">
         <DashboardNavbar />
+
         <main className="min-h-[calc(100vh-72px)] bg-[#f7f9fc]">
           <div className="p-8">
             <Outlet />
           </div>
         </main>
-      </SidebarInset>
-    </SidebarProvider>
+      </div>
+    </div>
   );
 }
